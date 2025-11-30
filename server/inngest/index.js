f50de13e3fdeffab489b6106ee86baab631b2454
Vignesh-vijaya <vigneshvijaya499@gmail.com>
2025-11-30 19:31:28 +0530

    update-workspace-inngest

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -90,5 +90,80 @@ const syncUserUpdation = inngest.createFunction(
   }
 );
 
+// Inngest function to save workspace data to a database
+const syncWorkspaceCreation = inngest.createFunction(
+  { id: "sync-workspace-from-clerk" },
+  { event: "clerk/organization.created" },
+  async ({ event }) => {
+    const { data } = event;
+    await prisma.workspace.create({
+        data: { 
+            id: data.id,
+            name: data.name,
+            slug: data.slug,
+            ownerId: data.owner_id,
+            image_url: data.image_url || null,
+            },
+    });
+//Add creator as ADMIN member
+    await prisma.workspaceMember.create({
+        data: {
+            userId: data.created_by,
+            workspaceId: data.id,
+            role: "ADMIN",
+        },
+    });     
+  }
+);
+
+//Inngest Functions to Update workspace data in database on update and delete events can be added similarly
+const syncWorkspaceUpdation = inngest.createFunction(
+  { id: "update-workspace-from-clerk" },
+  { event: "clerk/organization.updated" },
+  async ({ event }) => {
+    const { data } = event;
+    await prisma.workspace.update({
+        where: { id: data.id },
+        data: { name: data.name, slug: data.slug, image_url: data.image_url || null },
+    });
+  }
+  )
+
+  //inngest function to delete workspace from database on deletion event
+const syncWorkspaceDeletion = inngest.createFunction(
+  { id: "delete-workspace-from-clerk" }, 
+  { event: "clerk/organization.deleted" },
+  async ({ event }) => {
+    const { data } = event;
+    await prisma.workspace.delete({
+        where: { id: data.id },
+    });
+  }
+);
+
+//inngest function to save workspace member data to database on member addition event
+const syncWorkspaceMemberAddition = inngest.createFunction(
+  { id: "add-workspace-member-from-clerk" },
+    { event: "clerk/organization_member.created" },
+    async ({ event }) => {
+      const { data } = event;
+      await prisma.workspaceMember.create({
+        data: {
+          userId: data.user_id,
+          workspaceId: data.organization_id,
+          role: String(data.role_name).toUpperCase(), // Ensure role is in uppercase
+        },
+      });
+    }
+);
+
 // Export functions list so your deployment entrypoint / build can import them
-export const functions = [syncUserCreation, syncUserDeletion, syncUserUpdation];
+export const functions = [
+    syncUserCreation, 
+    syncUserDeletion, 
+    syncUserUpdation, 
+    syncWorkspaceCreation, 
+    syncWorkspaceUpdation, 
+    syncWorkspaceDeletion, 
+    syncWorkspaceMemberAddition
+];
